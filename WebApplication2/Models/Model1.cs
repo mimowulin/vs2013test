7f762775bbb08b307f938bda740d1cf8c0888eac
mimo <mimowulin@gmail.com>
2015-04-02 21:01:40 +0800

    新增留言板

---
+++ WebApplication2/Models/Model1.cs
@@ -0,0 +1,21 @@
+namespace WebApplication2.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Table> Table { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
